8b8c0201aab7e46db49c178b75cc7ae24498cc35
Vincent <yong72426@gmail.com>
2026-06-24 17:47:06 +0800

    finished assignment5 part1,2,3

---
+++ cs106-fa21-lib/CS106/build/Qt_6_5_3_for_macOS-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Wed Jun 24 16:15:53 2026"
+#define SPL_BUILD_USER "taoyongli"
+
+#endif
